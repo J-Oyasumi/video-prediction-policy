e8e071b1d9777323a40ed102c791570987a97fbd
hanjiang <hanjiang00@hotmail.com>
2026-02-23 18:27:55 +0800

    Add script to prepare latent tasks for Robocasa; iterates through predefined tasks and executes the preparation script for each task.

---
+++ prepare_latent_robocasa.sh
@@ -0,0 +1,30 @@
+#!bin/bash
+#SBATCH -J vpp_prepare_latent
+#SBATCH -o logs/%x-%j.out
+#SBATCH -e logs/%x-%j.err
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --time=12:00:00
+#SBATCH --partition=b200-mig45
+
+eval "$(conda shell.bash hook)"
+conda activate vpp
+
+tasks=(
+    "OpenCabinet" 
+    "CloseBlenderLid"
+    "CloseDrawer"
+    "OpenDrawer"
+    "PickPlaceCounterToCabinet"
+    "PickPlaceStoveToCounter"
+    "SlideDishwasherRack"
+    "CloseCabinet"
+    "OpenCabinet"
+    "PickPlaceCabinetToCounter"
+    "PickPlaceCounterToSink"
+    "PickPlaceToasterToCounter"
+)
+
+for task in ${tasks[@]}; do
+    python prepare_latent_robocasa.py --task_name $task
+done
